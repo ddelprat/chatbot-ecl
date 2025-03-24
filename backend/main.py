8b95a4689aa6db2220a0fa7bddc013a976801d3d
zhengfei-zhang <zhengfeizhang2011@gmail.com>
2025-03-24 22:42:25 +0100

    Save / Load embeddings in backend Optimizations

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,13 @@
 from flask import Flask, request, jsonify
+import re
+import csv
+import os
+script_dir = os.path.dirname(os.path.abspath(__file__))
+print("Current Directory:", os.chdir(script_dir))
+
+
+print(os.getcwd())
+
 from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 from sentence_transformers import SentenceTransformer
 import torch
@@ -15,20 +24,29 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 
 app = Flask(__name__)
 
-
 def compute_embeddings():
     model_embedding = SentenceTransformer("jinaai/jina-embeddings-v3", trust_remote_code=True)
-
+    # model_embedding = SentenceTransformer("paraphrase-multilingual-MiniLM-L12-v2", trust_remote_code=True)
     file_path = "processed_text.txt"
     with open(file_path, "r", encoding="utf-8") as file:
         passages = [line.strip() for line in file if line.strip()]
 
-    doc_embeddings = model_embedding.encode(passages, task="retrieval.passage")
-
+    file_exists = os.path.isfile('embeddings.csv')
+    if file_exists:
+        print("Embedding file already exists, reading...")
+        with open('embeddings.csv', 'r', encoding='utf-8') as f:
+            reader = csv.reader(f)
+            doc_embeddings = [[float(cell) for cell in row] for row in reader]
+    else:
+        print("Embedding file does not exist, creating...")
+        doc_embeddings = model_embedding.encode(passages)  # , task="retrieval.passage")
+        with open('embeddings.csv', 'w', newline='', encoding='utf-8') as f:
+            writer = csv.writer(f)
+            writer.writerows(doc_embeddings)
     return model_embedding, passages, doc_embeddings
 
 def retrieve_relevant_passages(query):
-    query_embedding = model_embedding.encode(query, task="retrieval.query")
+    query_embedding = model_embedding.encode(query) #, task="retrieval.query")
     similarities = model_embedding.similarity(query_embedding, doc_embeddings)[0]
     top_k = 3
     top_k_indices = torch.topk(similarities, k=top_k, dim=0).indices.tolist()
@@ -39,6 +57,7 @@ def retrieve_relevant_passages(query):
 
 def get_answer(question):
     context = retrieve_relevant_passages(question, )
+    print()
     response = generate_answer(question, context)
 
     def keep_after_keyword(text, keyword):
@@ -48,7 +67,7 @@ def get_answer(question):
     print("Question:", question)
     print("Réponse:", keep_after_keyword(response, "Réponse: "))
 
-    return keep_after_keyword(response, "Réponse: ")
+    return re.split(r'(?<=[.!?])\s', keep_after_keyword(response, "Réponse: "))[0]
 
 def generate_answer(question, context):
     prompt = f"""
@@ -65,6 +84,7 @@ def generate_answer(question, context):
     Réponse:"""
 
     # Tokenize & Generate Response
+    global tokenizer, model  # Utilisation des variables globales déjà chargées
     inputs = tokenizer(prompt, return_tensors="pt").to(device)
     output = model.generate(
         **inputs,
@@ -75,7 +95,7 @@ def generate_answer(question, context):
         repetition_penalty=1.3,
         pad_token_id=tokenizer.eos_token_id
     )
-
+    #return re.split(r'(?<=[.!?])\s', tokenizer.decode(output[0], skip_special_tokens=True))[0]
     return tokenizer.decode(output[0], skip_special_tokens=True)
 
 
@@ -94,7 +114,7 @@ def chat():
 
 if __name__ == "__main__":
     model_name = "mistralai/Mistral-7B-Instruct-v0.2"
-
+    print(device)
     print("Start embedding...")
     model_embedding, passages, doc_embeddings = compute_embeddings()
     print("Embedding done.")
@@ -113,9 +133,9 @@ if __name__ == "__main__":
     model = AutoModelForCausalLM.from_pretrained(
         model_name,
         device_map=device,
-        # quantization_config=quantization_config
+        quantization_config=quantization_config
     )
 
     print("LLM loaded successfully with 4-bit quantization!")
 
-    app.run(host="0.0.0.0", port=8080, debug=False)
+    app.run(host="0.0.0.0", port=8080, debug=False, threaded=True)
